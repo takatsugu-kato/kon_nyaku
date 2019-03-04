7e5eb123cc7514296a718717210595c66562c7f1
Takatsugu Kato <kato@ideainstitute.co.jp>
2019-03-04 11:05:43 +0900

    complete the Back_to_xlf function

--- xlf.py
+++ xlf.py
@@ -3,43 +3,54 @@ import re
 
 class Xlf():
     def __init__(self, path):
+        self.source_language = ""
+        self.target_language = ""
+
         self.path = path
         self.tree = ET.parse(path)
+        ET.register_namespace('', 'urn:oasis:names:tc:xliff:document:1.2')
+        ET.register_namespace('okp', 'okapi-framework:xliff-extensions')
+        ET.register_namespace('its', 'http://www.w3.org/2005/11/its')
+        ET.register_namespace('itsxlf', 'http://www.w3.org/ns/its-xliff/')
         self.root = self.tree.getroot()
         self.ns = {
             'xliff': 'urn:oasis:names:tc:xliff:document:1.2',
-            'okp': 'okapi-framework:xliff-extensions',
-            'its': 'http://www.w3.org/2005/11/its',
-            'itsxlf': 'http://www.w3.org/ns/its-xliff/'
         }
-        self.seg_data = self.get_segment()
-    def get_segment(self):
+        self.seg_data = self.Get_segment()
+        self.Set_language()
+
+    def Set_language(self):
+        files = self.root.findall('xliff:file', self.ns)
+        self.source_language = files[0].get('source-language')
+        self.target_language = files[0].get('target-language')
+
+    def Get_segment(self):
         files = []
         for file in self.root.findall('xliff:file', self.ns):
             file_obj = File(file.get('original'))
             for trans_unit_element in file.findall('xliff:body/xliff:trans-unit', self.ns):
                 trans_unit_obj = TransUnit(trans_unit_element.get('id'))
 
-                trans_unit_obj.source = create_seg_obj(self,trans_unit_element, "source")
-                trans_unit_obj.seg_source = create_seg_obj(self, trans_unit_element, "seg-source")
-                trans_unit_obj.seg_target = create_seg_obj(self, trans_unit_element, "target")
+                trans_unit_obj.source = Create_seg_obj(self,trans_unit_element, "source")
+                trans_unit_obj.seg_source = Create_seg_obj(self, trans_unit_element, "seg-source")
+                trans_unit_obj.seg_target = Create_seg_obj(self, trans_unit_element, "target")
 
                 file_obj.segments.append(trans_unit_obj)
             files.append(file_obj)
         return files
 
-    def back_to_xlf(self):
+    def Back_to_xlf(self):
         for file in self.seg_data:
             for trans_unit in file.segments:
-                a = create_segment_element(self, trans_unit.seg_target)
-                string = ET.tostring(a, encoding='unicode', short_empty_elements=True)
-                print (string)
-                condition = 'xliff:file[@original="{0}"]/xliff:body/xliff:trans-unit[@id="{1}"]/xliff:target'.format(file.original, trans_unit.id)
-                trans_unit_target = self.root.find(condition, self.ns)
-                # self.root.remove(trans_unit_target)
-                trans_unit_target.append(a)
-        self.tree.write('output.xml')
-        # ET.dump(self.tree)
+                new_target_element = Create_segment_element(self, trans_unit.seg_target)
+                # string = ET.tostring(new_target_element, encoding='unicode', short_empty_elements=True)
+                # print (string)
+                condition = 'xliff:file[@original="{0}"]/xliff:body/xliff:trans-unit[@id="{1}"]'.format(file.original, trans_unit.id)
+                trans_unit = self.root.find(condition, self.ns)
+                target = trans_unit.find('xliff:target', self.ns)
+                trans_unit.remove(target)
+                trans_unit.append(new_target_element)
+        self.tree.write('output.xml', encoding="utf-8")
 
 class File():
     def __init__(self, original):
@@ -58,16 +69,18 @@ class Segment():
         self.id = id
         self.string = ""
 
-def create_segment_element(self,segment_obj):
-    root = ET.Element('root')
+def Create_segment_element(self,segment_obj):
+    root = ET.Element('target')
+    root.set("xml:lang", self.target_language)
     for mrk in segment_obj:
         mrk_element = ET.SubElement(root, 'mrk')
         mrk_element.set("mid", mrk.id)
+        mrk_element.set("mtype", "seg")
+        ##ToDO XML文字をでコードして入れる方法を探す
         mrk_element.text = mrk.string
-    # print (root.text)
     return root
 
-def create_seg_obj(self, trans_unit_element, tag):
+def Create_seg_obj(self, trans_unit_element, tag):
     element = trans_unit_element.find('xliff:'+tag, self.ns)
 
     if (tag == "source"):
@@ -91,10 +104,9 @@ def Clean_element_string(string):
     string = re.sub('</ns[0-9]+:', "</", string)
     string = re.sub('</?source.*?>', "", string)
     string = re.sub('</?mrk.*?>', "", string)
-    # string = re.sub('</?seg-source.*?>', "", string)
     return string
 
 xlf_obj = Xlf("./sample_file/test.docx.xlf")
 xlf_obj.seg_data[1].segments[0].seg_target[0].string = "irekaemasita"
 
-xlf_obj.back_to_xlf()
+xlf_obj.Back_to_xlf()
