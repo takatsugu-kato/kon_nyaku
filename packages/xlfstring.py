ea3760abbcb723e725b8f78605773bc8396ecf25
Takatsugu Kato <kato@ideainstitute.co.jp>
2019-03-06 11:42:24 +0900

    update xlfstring class

--- packages/xlfstring.py
+++ packages/xlfstring.py
@@ -30,7 +30,7 @@ class XlfString():
             deleted_inline_tag_string = repatter.sub("\\1", deleted_inline_tag_string)
         return deleted_inline_tag_string
 
-    def change_inline_tag(self):
+    def change_xlf_inline_tag_to_span(self):
         """
         Change xlf inline tag to span tag
         
@@ -40,8 +40,28 @@ class XlfString():
 
         changed_inline_tag_string = self.string
         for tag in self.inline_tag_list:
-            repatter = re.compile(r'<{0} id="(.*?)">(.*?)</{0}>'.format(tag))
-            changed_inline_tag_string = repatter.sub('<span id="\\1">\\2</span>', changed_inline_tag_string)
+            repatter = re.compile(r'<{0} id="(.*?)">'.format(tag))
+            changed_inline_tag_string = repatter.sub('<span id="\\1">', changed_inline_tag_string)
+            repatter = re.compile(r'</{0}>'.format(tag))
+            changed_inline_tag_string = repatter.sub('</span>', changed_inline_tag_string)
+        return changed_inline_tag_string
+
+    def change_span_to_xlf_inline_tag(self, text):
+        """
+        Change span tag to xlf inline tag
+
+        Args:
+            text (str): String of to change span tag to xlf tag
+        
+        Returns:
+            str: string of changed span tag to xlf tag
+        """
+
+        changed_inline_tag_string = text
+        repatter = re.compile(r'<{0} id="(.*?)">'.format("span"))
+        changed_inline_tag_string = repatter.sub('<g id="\\1">', changed_inline_tag_string)
+        repatter = re.compile(r'</{0}>'.format("span"))
+        changed_inline_tag_string = repatter.sub('</g>', changed_inline_tag_string)
         return changed_inline_tag_string
 
     def __void_paired_placeholder(self):
