da9dae34b48a3223d9647f5f4d3a87e5800d6e85
Takatsugu Kato <kato@ideainstitute.co.jp>
2019-03-11 10:19:11 +0900

    add translate file script

---
+++ transaltefile.py
@@ -0,0 +1,28 @@
+"""
+This is sample program to use xlf and okapi modules
+"""
+
+from packages.xlf import Xlf
+from packages.okapi import Okapi
+
+def translate_xlf():
+    """
+    Translate xlf file
+    """
+
+    source_lang = "en"
+    target_lang = "ja"
+    model = "nmt"
+    to_trans_file = "./sample_file/test.docx"
+
+    okapi_obj = Okapi(source_lang, target_lang)
+    okapi_obj.create_xlf(to_trans_file)
+
+
+    xlf_obj = Xlf(to_trans_file + ".xlf")
+    xlf_obj.translate(model, delete_format_tag=True)
+    xlf_obj.back_to_xlf()
+
+    okapi_obj.create_transled_file(to_trans_file + ".xlf")
+
+translate_xlf()
