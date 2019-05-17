937a900f0d7e689d86c80921ffcc911bd6aec6cd
Takatsugu Kato <kato@ideainstitute.co.jp>
2019-05-17 13:47:41 +0900

    update field name

--- translate/models.py
+++ translate/models.py
@@ -3,15 +3,15 @@ from django.utils import timezone
 
 class File(models.Model):
     """File"""
-    name = models.CharField('ファイル名', max_length=255)
+    name = models.CharField('File name', max_length=255)
     file_session_key = models.CharField('Session Key', max_length=40, default='')
-    status = models.IntegerField('ステータス', blank=True, default=0)
-    source_lang = models.CharField('ソース言語', max_length=8, default='')
-    target_lang = models.CharField('ターゲット言語', max_length=8, default='')
-    progress = models.IntegerField('進捗', blank=True, default=0)
-    created_date = models.DateTimeField('作成日', auto_now_add=True)
-    modified_date = models.DateTimeField('変更日', auto_now=True)
-    document = models.FileField('ファイル', upload_to='media/')
+    status = models.IntegerField('Status', blank=True, default=0)
+    source_lang = models.CharField('Source Language', max_length=8, default='')
+    target_lang = models.CharField('Target Language', max_length=8, default='')
+    progress = models.IntegerField('Progress', blank=True, default=0)
+    created_date = models.DateTimeField('Created', auto_now_add=True)
+    modified_date = models.DateTimeField('Modified', auto_now=True)
+    document = models.FileField('File', upload_to='media/')
 
     def __str__(self):
         return self.name
