0cff2d8764e0c3d3c0bb5115797c57659321ba98
Takatsugu Kato <kato@ideainstitute.co.jp>
2019-05-30 16:44:26 +0900

    change view name

--- translate/urls.py
+++ translate/urls.py
@@ -3,10 +3,9 @@ from translate import views
 
 app_name = 'translate'
 urlpatterns = [
-    # 書籍
-    path('file/', views.translator, name='translator'),   # Translation view
-    path('file/tra/<int:file_id>/', views.file_tra, name='file_tra'),  # Translate
-    path('file/del/<int:file_id>/', views.file_del, name='file_del'),   # Delete
+    path('translator/', views.translator, name='translator'),   # Translation view
+    path('translator/tra/<int:file_id>/', views.file_tra, name='file_tra'),  # Translate
+    path('translator/del/<int:file_id>/', views.file_del, name='file_del'),   # Delete
     path('get_file_list_data/', views.get_file_list_data),
     path('upload_file/', views.upload_file),
     path('translate_text/', views.translate_text),
